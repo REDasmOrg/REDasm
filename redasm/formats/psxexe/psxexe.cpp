cb6e54ac3a7f1b6bb119202b54e9b17b6cf90cff
Dax89 <trogu.davide@gmail.com>
2017-12-28 19:04:15 +0100

    PSX-EXE Format: Extended segment information

--- redasm/formats/psxexe/psxexe.cpp
+++ redasm/formats/psxexe/psxexe.cpp
@@ -2,8 +2,10 @@
 #include "psxexe_analyzer.h"
 #include <cstring>
 
-#define PSXEXE_SIGNATURE      "PS-X EXE"
-#define PSXEXE_TEXT_OFFSET    0x00000800
+#define PSXEXE_SIGNATURE   "PS-X EXE"
+#define PSXEXE_TEXT_OFFSET 0x00000800
+#define PSX_USER_RAM_START 0x80000000
+#define PSX_USER_RAM_END   0x80200000
 
 namespace REDasm {
 
@@ -39,8 +41,15 @@ bool PsxExeFormat::load(u8* rawformat)
     if(strncmp(format->id, PSXEXE_SIGNATURE, PSXEXE_SIGNATURE_SIZE))
         return false;
 
+    if(format->t_addr > PSX_USER_RAM_START)
+        this->defineSegment("RAM0", 0, PSX_USER_RAM_START, (format->t_addr - PSX_USER_RAM_START), SegmentTypes::Data | SegmentTypes::Bss);
+
     this->addSignature("psyq47");
     this->defineSegment("TEXT", PSXEXE_TEXT_OFFSET, format->t_addr, format->t_size, SegmentTypes::Code | SegmentTypes::Data);
+
+    if((format->t_addr + format->t_size) < PSX_USER_RAM_END)
+        this->defineSegment("RAM1", 0, format->t_addr + format->t_size, PSX_USER_RAM_END - (format->t_addr + format->t_size), SegmentTypes::Data | SegmentTypes::Bss);
+
     this->defineEntryPoint(format->pc0);
 
     FormatPluginT<PsxExeHeader>::load(rawformat);

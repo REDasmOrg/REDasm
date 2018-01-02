ee7703e43d80d04c7ec2676988c8f65503fac562
Dax89 <trogu.davide@gmail.com>
2018-01-02 12:41:06 +0100

    Buffer: Added decrement operators

--- redasm/redasm.h
+++ redasm/redasm.h
@@ -142,9 +142,13 @@ struct Buffer
     Buffer(u8* data, u64 length): data(data), length(length) { }
     Buffer(char* data, u64 length): data(reinterpret_cast<u8*>(data)), length(length) { }
     Buffer operator +(u64 v) const { Buffer b(data, length); b.data += v; b.length -= v; return b; }
+    Buffer operator -(u64 v) const { Buffer b(data, length); b.data -= v; b.length += v; return b; }
     Buffer& operator +=(u64 v) { data += v; length -= v; return *this; }
+    Buffer& operator -=(u64 v) { data += v; length += v; return *this; }
     Buffer operator++(int) { Buffer copy = *this; *this += 1; return copy; }
     Buffer& operator++() { *this += 1; return *this; }
+    Buffer operator--(int) { Buffer copy = *this; *this -= 1; return copy; }
+    Buffer& operator--() { *this -= 1; return *this; }
     u8 operator[](int index) const { return data[index]; }
     u8 operator*() const { return *data; }
     bool eob() const { return !length; }

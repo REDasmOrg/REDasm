ad389125a7b559cec5a3eba6d11eec86bca8b797
Dax89 <trogu.davide@gmail.com>
2018-01-07 15:33:54 +0100

    AppVeyor: Updated build scripts

--- ci/deploy.ps1
+++ ci/deploy.ps1
@@ -8,7 +8,7 @@ if(Test-Path $($env:build_repo)) {
     Remove-Item $($env:build_repo) -Recurse -Force 
 }
 
-git clone -b builds https://$($env:github_token)@github.com/REDasm/$($env:build_repo).git > $null 2>&1
+git clone -b nightly https://$($env:github_token)@github.com/REDasm/$($env:build_repo).git > $null 2>&1
 cd $($env:build_repo)
 Remove-Item *Windows* 
     
@@ -19,5 +19,5 @@ if(Test-Path ../$archive)
     git config --global user.name "AppVeyor Build Bot"
     git add -A .
     git commit -m "Updated Windows Nightly $(Get-Date -format ddMMyyyy)"
-    git push --quiet origin builds > $null 2>&1 
+    git push --quiet origin nightly > $null 2>&1 
 }

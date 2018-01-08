851ab4743f9824a56ad77f000f8a4ed8a8266370
Dax89 <trogu.davide@gmail.com>
2018-01-08 14:56:36 +0100

    DalvikPrinter: Display extended debug info (locals, lines) if any

--- redasm/assemblers/dalvik/dalvik_printer.cpp
+++ redasm/assemblers/dalvik/dalvik_printer.cpp
@@ -20,8 +20,12 @@ void DalvikPrinter::header(const SymbolPtr &symbol, Printer::HeaderCallback head
         return;
     }
 
-    DEXEncodedMethod dexmethod;
+    // Reset printer data
+    this->_regnames.clear();
+    this->_regoverrides.clear();
+    this->_currentdbginfo.line_start = DEX_NO_INDEX_U;
 
+    DEXEncodedMethod dexmethod;
     std::string access;
 
     if(dexformat->getMethodInfo(symbol->extra_type, dexmethod))
@@ -46,7 +50,7 @@ void DalvikPrinter::header(const SymbolPtr &symbol, Printer::HeaderCallback head
                symbol->name, dexformat->getParameters(symbol->extra_type));
 }
 
-void DalvikPrinter::prologue(const SymbolPtr &symbol, Printer::PrologueCallback prologuefunc)
+void DalvikPrinter::prologue(const SymbolPtr &symbol, Printer::LineCallback prologuefunc)
 {
     DEXFormat* dexformat = dynamic_cast<DEXFormat*>(this->_disassembler->format());
 
@@ -58,23 +62,71 @@ void DalvikPrinter::prologue(const SymbolPtr &symbol, Printer::PrologueCallback 
     if(!dexformat->getMethodInfo(symbol->extra_type, dexmethod))
         return;
 
-    DEXDebugInfo dexdebuginfo;
-
-    if(!dexformat->getDebugInfo(symbol->extra_type, dexdebuginfo))
+    if(!dexformat->getDebugInfo(symbol->extra_type, this->_currentdbginfo))
         return;
 
     u32 delta = (dexmethod.access_flags & DexAccessFlags::Static) ? 0 : 1;
 
-    for(auto it = dexdebuginfo.parameter_names.begin(); it != dexdebuginfo.parameter_names.end(); it++)
+    for(auto it = this->_currentdbginfo.parameter_names.begin(); it != this->_currentdbginfo.parameter_names.end(); it++)
     {
-        u32 argidx = std::distance(dexdebuginfo.parameter_names.begin(), it) + delta;
+        u32 argidx = std::distance(this->_currentdbginfo.parameter_names.begin(), it) + delta;
         prologuefunc(".arg" + std::to_string(argidx) + ": " + *it);
+    }
+}
+
+void DalvikPrinter::info(const InstructionPtr &instruction, Printer::LineCallback infofunc)
+{
+    if(this->_currentdbginfo.line_start == DEX_NO_INDEX_U)
+        return;
+
+    auto dbgit = this->_currentdbginfo.debug_data.find(instruction->address);
+
+    if(dbgit == this->_currentdbginfo.debug_data.end())
+        return;
+
+    DEXFormat* dexformat = dynamic_cast<DEXFormat*>(this->_disassembler->format());
+
+    if(!dexformat)
+        return;
+
+    for(auto it = dbgit->second.begin(); it != dbgit->second.end(); it++)
+    {
+        const DEXDebugData& debugdata = *it;
+
+        if((debugdata.data_type == DEXDebugDataTypes::StartLocal) || ((debugdata.data_type == DEXDebugDataTypes::StartLocalExtended)))
+        {
+            if(debugdata.name_idx == DEX_NO_INDEX)
+                continue;
+
+            this->startLocal(dexformat, debugdata);
+            std::string name = dexformat->getString(debugdata.name_idx), type;
+
+            if(debugdata.type_idx != DEX_NO_INDEX)
+                type = ": " + dexformat->getType(debugdata.type_idx);
+
+            infofunc(".local " + this->registerName(debugdata.register_num) + " = " + name + type);
+        }
+        else if(debugdata.data_type == DEXDebugDataTypes::RestartLocal)
+            this->restoreLocal(dexformat, debugdata.register_num);
+        else if(debugdata.data_type == DEXDebugDataTypes::EndLocal)
+            this->endLocal(debugdata.register_num);
+        else if(debugdata.data_type == DEXDebugDataTypes::PrologueEnd)
+            infofunc(".prologue_end");
+        else if(debugdata.data_type == DEXDebugDataTypes::Line)
+            infofunc(".line " + std::to_string(debugdata.line_no));
     }
 }
 
 std::string DalvikPrinter::reg(const RegisterOperand &regop) const
 {
-    std::string s = "v" + std::to_string(regop.r);
+    std::string s;
+
+    auto it = this->_regnames.find(regop.r);
+
+    if(it != this->_regnames.end())
+        s = it->second;
+    else
+        s = DalvikPrinter::registerName(regop.r);
 
     if(regop.extra_type & DalvikOperands::ParameterFirst)
         s = "{" + s;
@@ -111,6 +163,33 @@ std::string DalvikPrinter::imm(const Operand &op) const
     }
 
     return Printer::imm(op);
+}
+
+std::string DalvikPrinter::registerName(register_t r) { return "v" + std::to_string(r); }
+
+void DalvikPrinter::startLocal(DEXFormat* dexformat, const DEXDebugData &debugdata)
+{
+    this->_regoverrides[debugdata.register_num] = debugdata;
+    this->_regnames[debugdata.register_num] = dexformat->getString(debugdata.name_idx);
+}
+
+void DalvikPrinter::restoreLocal(DEXFormat* dexformat, register_t r)
+{
+    auto it = this->_regoverrides.find(r);
+
+    if(it == this->_regoverrides.end())
+        return;
+
+    const DEXDebugData& debugdata = it->second;
+    this->_regnames[r] = dexformat->getString(debugdata.name_idx);
+}
+
+void DalvikPrinter::endLocal(register_t r)
+{
+    auto it = this->_regnames.find(r);
+
+    if(it != this->_regnames.end())
+        this->_regnames.erase(it);
 }
 
 } // namespace REDasm
--- redasm/assemblers/dalvik/dalvik_printer.h
+++ redasm/assemblers/dalvik/dalvik_printer.h
@@ -2,17 +2,32 @@
 #define DALVIK_PRINTER_H
 
 #include "../../plugins/assembler/printer.h"
+#include "../../formats/dex/dex_header.h"
 
 namespace REDasm {
+
+class DEXFormat;
 
 class DalvikPrinter : public Printer
 {
     public:
         DalvikPrinter(DisassemblerFunctions* disassembler, SymbolTable* symboltable);
         virtual void header(const SymbolPtr &symbol, HeaderCallback plgfunc);
-        virtual void prologue(const SymbolPtr &symbol, PrologueCallback prologuefunc);
+        virtual void prologue(const SymbolPtr &symbol, LineCallback prologuefunc);
+        virtual void info(const InstructionPtr &instruction, LineCallback infofunc);
         virtual std::string reg(const RegisterOperand &regop) const;
         virtual std::string imm(const Operand& op) const;
+
+    private:
+        static std::string registerName(register_t r);
+        void startLocal(REDasm::DEXFormat *dexformat, const DEXDebugData& debugdata);
+        void restoreLocal(REDasm::DEXFormat *dexformat, register_t r);
+        void endLocal(register_t r);
+
+    private:
+        DEXDebugInfo _currentdbginfo;
+        std::unordered_map<u16, DEXDebugData> _regoverrides;
+        std::unordered_map<u16, std::string> _regnames;
 };
 
 } // namespace REDasm

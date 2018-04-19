43d1026c3e01024f86e5372d03b08e89de48b122
Dax89 <trogu.davide@gmail.com>
2018-04-19 14:17:05 +0200

    GBA Format: Extended Segmentation

--- redasm/formats/gba/gba.cpp
+++ redasm/formats/gba/gba.cpp
@@ -2,14 +2,33 @@
 #include "gba_analyzer.h"
 #include <cstring>
 
-#define GBAEWRAM_START_ADDR  0x02000000
-#define GBA_EWROM_SIZE       0x00030000
+// https://www.reinterpretcast.com/writing-a-game-boy-advance-game
 
-#define GBA_IWRAM_START_ADDR 0x03000000
-#define GBA_IWRAM_SIZE       0x00007FFF
+#define GBA_SYSROM_START_ADDR  0x00000000
+#define GBA_SYSROM_SIZE        0x00003FFF
 
-#define GBA_ROM_START_ADDR   0x08000000
-#define GBA_ROM_SIZE         0x02000000
+#define GBA_EWRAM_START_ADDR   0x02000000
+#define GBA_EWRAM_SIZE         0x00030000
+
+#define GBA_IWRAM_START_ADDR   0x03000000
+#define GBA_IWRAM_SIZE         0x00007FFF
+
+#define GBA_IOREG_START_ADDR   0x04000000
+#define GBA_IOREG_SIZE         0x000003FF
+
+#define GBA_PALETTE_START_ADDR 0x05000000
+#define GBA_PALETTE_SIZE       0x000003FF
+
+#define GBA_VRAM_START_ADDR    0x06000000
+#define GBA_VRAM_SIZE          0x00017FFF
+
+#define GBA_OAMRAM_START_ADDR  0x07000000
+#define GBA_OAMRAM_SIZE        0x000003FF
+
+#define GBA_ROM_START_ADDR     0x08000000
+#define GBA_ROM_SIZE           0x02000000
+
+#define GBA_SEGMENT_AREA(name) GBA_##name##_START_ADDR, GBA_##name##_SIZE
 
 namespace REDasm {
 
@@ -50,8 +69,13 @@ bool GbaRomFormat::load(u8* rawformat, u64 length)
     if(!(format->fixed_val == 0x96)) // no signature/magic number is present in GBA ROMS
         return false;
 
-    this->defineSegment("EWRAM", 0, GBAEWRAM_START_ADDR, GBA_EWROM_SIZE, SegmentTypes::Bss);
-    this->defineSegment("IWRAM", 0, GBA_IWRAM_START_ADDR, GBA_IWRAM_SIZE, SegmentTypes::Bss);
+    this->defineSegment("SYSROM", 0, GBA_SEGMENT_AREA(SYSROM), SegmentTypes::Bss);
+    this->defineSegment("EWRAM", 0, GBA_SEGMENT_AREA(EWRAM), SegmentTypes::Bss);
+    this->defineSegment("IWRAM", 0, GBA_SEGMENT_AREA(IWRAM), SegmentTypes::Bss);
+    this->defineSegment("IOREG", 0, GBA_SEGMENT_AREA(IOREG), SegmentTypes::Bss);
+    this->defineSegment("PALETTE", 0, GBA_SEGMENT_AREA(PALETTE), SegmentTypes::Bss);
+    this->defineSegment("VRAM", 0, GBA_SEGMENT_AREA(VRAM), SegmentTypes::Bss);
+    this->defineSegment("OAMRAM", 0, GBA_SEGMENT_AREA(OAMRAM), SegmentTypes::Bss);
     this->defineSegment("ROM", 0, GBA_ROM_START_ADDR, length, SegmentTypes::Code | SegmentTypes::Data);
 
     this->defineEntryPoint(GBA_ROM_START_ADDR); // Let REDasm decode and follow the "EP Field"

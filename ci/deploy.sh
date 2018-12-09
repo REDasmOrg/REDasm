43126887dfd263d954d464938c70720b42b8976f
Dax <trogu.davide@gmail.com>
2018-12-09 20:21:47 +0100

    Travis CI: Updated Scripts

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 
-DEPLOY_DIR=deploy
 OS_NAME=`uname`
 ARCH=`uname -m`
 BUILD_DATE=`date +%d%m%Y`
@@ -8,26 +7,12 @@ BUILD_ID="REDasm_"$OS_NAME"_"$ARCH"_"$BUILD_DATE
 BUILD_ORG="REDasmOrg"
 BUILD_REPO="REDasm-Builds"
 
-mkdir $DEPLOY_DIR
-cp REDasm $DEPLOY_DIR  # Copy executable
-cp -r database $DEPLOY_DIR/  # Copy database
-
-cd $DEPLOY_DIR
 zip -r ../$BUILD_ID.zip * # Generate archive
 cd ..
-
-# Cleanup temporary files
-rm -rf database/
-rm -rf capstone/
-rm -rf ogdf/
-rm -rf *.o
-rm Makefile
-rm -rf $DEPLOY_DIR
 
 # Prepare deploy
 rm -rf $BUILD_REPO
 git clone -b nightly https://${GITHUB_TOKEN}@github.com/$BUILD_ORG/$BUILD_REPO.git > /dev/null 2>&1
-
 cd $BUILD_REPO
 rm -rf *$OS_NAME*
 

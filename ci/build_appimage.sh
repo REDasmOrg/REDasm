a6f81435d30547016a484b1631dda3b23d8b4055
Dax <trogu.davide@gmail.com>
2018-12-17 15:41:06 +0100

    Updated build scripts

---
+++ ci/build_appimage.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+LINUXDEPLOYQT_ARCH=`uname -m`
+LINUXDEPLOYQT_VER="continuous"
+LINUXDEPLOYQT_CMD="linuxdeployqt-$LINUXDEPLOYQT_VER-$LINUXDEPLOYQT_ARCH.AppImage"
+LINUXDEPLOYQT_URL="https://github.com/probonopd/linuxdeployqt/releases/download/$LINUXDEPLOYQT_VER/$LINUXDEPLOYQT_CMD"
+
+cd ..
+
+if [ ! -f $LINUXDEPLOYQT_CMD ]; then
+    wget -c $LINUXDEPLOYQT_URL
+fi
+
+if [ -f $LINUXDEPLOYQT_CMD ]; then
+    chmod a+x ./$LINUXDEPLOYQT_CMD
+    cd deploy
+    ../$LINUXDEPLOYQT_CMD REDasm -appimage -exclude-libs=libnss.so,libnssutil3.so
+    mv REDasm-*.AppImage ../
+    cd ..
+else
+    echo "$LINUXDEPLOYQT_CMD not found, skipping AppImage creation"
+fi
+
+cd build
---
+++ ci/prepare_appimage.py
@@ -0,0 +1,15 @@
+import shutil
+import os
+
+os.chdir("../deploy")
+
+if not os.path.exists("REDasm"):
+ print("Skipping AppImage creation")
+ exit(1)
+
+shutil.rmtree("lib", ignore_errors=True)
+os.mkdir("lib")
+shutil.move("LibREDasm.so", "lib/")
+
+shutil.copy("../artwork/logo.png", "./")
+shutil.copy("../ci/REDasm.desktop", "./")

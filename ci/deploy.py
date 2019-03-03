52ba9f8ecd6a2d8ee9cb8ca39874194d56f5da70
Dax <trogu.davide@gmail.com>
2019-03-03 14:59:33 +0100

    Deploy step: Removed unused vars, try with scp command

--- ci/deploy.py
+++ ci/deploy.py
@@ -8,7 +8,7 @@ def do_deploy(filename):
  fp = NamedTemporaryFile(mode="w", delete=False)
  fp.write(os.getenv("DEPLOY_TOKEN"))
  fp.close();
- subprocess.run(["sftp", filename, "-i", [fp.name], os.getenv("DEPLOY_DESTINATION")])
+ subprocess.run(["scp", "-i", [fp.name], filename, os.getenv("DEPLOY_DESTINATION")])
  os.remove(fp.name)
 
 os.chdir("..")
--- ci/deploy_vars.py
+++ ci/deploy_vars.py
@@ -20,10 +20,3 @@ BUILD_ID          = "REDasm_" + OS_NAME + "_" + ARCH  + "_" + BUILD_DATE
 BUILD_ARCHIVE     = BUILD_ID + ".zip"
 BUILD_REPO        = "REDasm-Builds"
 DATABASE_REPO_URL = "https://github.com/REDasmOrg/REDasm-Database.git"
-
-
-if os.getenv("GITHUB_TOKEN") == None:
-    print("WARNING: Invalid GH-Token")
-    BUILD_REPO_URL = "https://github.com/REDasmOrg/" + BUILD_REPO + ".git"
-else:
-    BUILD_REPO_URL = "https://" + os.getenv("GITHUB_TOKEN") + "@github.com/REDasmOrg/" + BUILD_REPO + ".git"

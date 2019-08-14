bcf4ef2181b724d3ed59c55302b110eac6fd039b
Dax <trogu.davide@gmail.com>
2019-08-14 21:09:54 +0200

    SegmentsModel: Display section coverage

--- models/segmentsmodel.cpp
+++ models/segmentsmodel.cpp
@@ -20,22 +20,20 @@ QVariant SegmentsModel::data(const QModelIndex &index, int role) const
         const REDasm::Assembler* assembler = m_disassembler->assembler();
         const REDasm::Segment* segment = variant_object<REDasm::Segment>(m_disassembler->document()->segments().at(index.row()));
 
-        if(index.column() == 0)
-            return Convert::to_qstring(REDasm::String::hex(segment->address, assembler->bits()));
-        if(index.column() == 1)
-            return Convert::to_qstring(REDasm::String::hex(segment->endaddress, assembler->bits()));
-        if(index.column() == 2)
-            return Convert::to_qstring(REDasm::String::hex(segment->size(), assembler->bits()));
-        if(index.column() == 3)
-            return Convert::to_qstring(REDasm::String::hex(segment->offset, assembler->bits()));
-        if(index.column() == 4)
-            return Convert::to_qstring(REDasm::String::hex(segment->endoffset, assembler->bits()));
-        if(index.column() == 5)
-            return Convert::to_qstring(REDasm::String::hex(segment->rawSize(), assembler->bits()));
-        if(index.column() == 6)
-            return Convert::to_qstring(segment->name);
-        if(index.column() == 7)
-            return SegmentsModel::segmentFlags(segment);
+        switch(index.column())
+        {
+            case 0: return Convert::to_qstring(REDasm::String::hex(segment->address, assembler->bits()));
+            case 1: return Convert::to_qstring(REDasm::String::hex(segment->endaddress, assembler->bits()));
+            case 2: return Convert::to_qstring(REDasm::String::hex(segment->size(), assembler->bits()));
+            case 3: return Convert::to_qstring(REDasm::String::hex(segment->offset, assembler->bits()));
+            case 4: return Convert::to_qstring(REDasm::String::hex(segment->endoffset, assembler->bits()));
+            case 5: return Convert::to_qstring(REDasm::String::hex(segment->rawSize(), assembler->bits()));
+            case 6: return Convert::to_qstring(segment->name);
+            case 7: return SegmentsModel::segmentFlags(segment);
+            case 8: return (segment->coveragebytes == REDasm::npos) ? "N/A" : (QString::number((static_cast<double>(segment->coveragebytes) /
+                                                                                                static_cast<double>(segment->rawSize())) * 100, 'g', 3) + "%");
+            default: break;
+        }
     }
     else if(role == Qt::ForegroundRole)
     {
@@ -62,27 +60,24 @@ QVariant SegmentsModel::headerData(int section, Qt::Orientation orientation, int
     if(orientation == Qt::Vertical || role != Qt::DisplayRole)
         return ListingItemModel::headerData(section, orientation, role);
 
-    if(section == 0)
-        return "Start Address";
-    if(section == 1)
-        return "End Address";
-    if(section == 2)
-        return "Size";
-    if(section == 3)
-        return "Offset";
-    if(section == 4)
-        return "End Offset";
-    if(section == 5)
-        return "Raw Size";
-    if(section == 6)
-        return "Name";
-    if(section == 7)
-        return "Type";
+    switch(section)
+    {
+        case 0: return "Start Address";
+        case 1: return "End Address";
+        case 2: return "Size";
+        case 3: return "Offset";
+        case 4: return "End Offset";
+        case 5: return "Raw Size";
+        case 6: return "Name";
+        case 7: return "Type";
+        case 8: return "Coverage";
+        default: break;
+    }
 
     return ListingItemModel::headerData(section, orientation, role);
 }
 
-int SegmentsModel::columnCount(const QModelIndex &) const { return 8; }
+int SegmentsModel::columnCount(const QModelIndex &) const { return 9; }
 
 QString SegmentsModel::segmentFlags(const REDasm::Segment *segment)
 {

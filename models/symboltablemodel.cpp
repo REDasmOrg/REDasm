40a2dfdfcb2ae6955f7a97adb58705179693cc79
Dax <trogu.davide@gmail.com>
2019-10-14 22:10:35 +0200

    SymbolTableModel: Check for SymbolType and SymbolFlags

--- models/symboltablemodel.cpp
+++ models/symboltablemodel.cpp
@@ -1,13 +1,17 @@
 #include "symboltablemodel.h"
+#include <redasm/context.h>
 
-SymbolTableModel::SymbolTableModel(REDasm::ListingItemType itemtype, QObject *parent) : ListingItemModel(itemtype, parent), m_symboltype(REDasm::SymbolType::None) { }
+SymbolTableModel::SymbolTableModel(REDasm::ListingItemType itemtype, QObject *parent) : ListingItemModel(itemtype, parent) { }
 void SymbolTableModel::setSymbolType(REDasm::SymbolType type) { m_symboltype = type; }
+void SymbolTableModel::setSymbolFlags(REDasm::SymbolFlags flags) { m_symbolflags = flags; }
 
 bool SymbolTableModel::isItemAllowed(const REDasm::ListingItem& item) const
 {
     if(!ListingItemModel::isItemAllowed(item) || (!item.is(REDasm::ListingItemType::FunctionItem) && !item.is(REDasm::ListingItemType::SymbolItem)))
         return false;
 
-    const REDasm::Symbol* symbol = m_disassembler->documentNew()->symbol(item.address_new);
-    return symbol && symbol->is(m_symboltype);
+    const REDasm::Symbol* symbol = r_docnew->symbol(item.address_new);
+    if(!symbol || !symbol->typeIs(m_symboltype)) return false;
+    if(m_symbolflags == REDasm::SymbolFlags::None) return true;
+    return symbol->hasFlag(m_symbolflags);
 }
--- models/symboltablemodel.h
+++ models/symboltablemodel.h
@@ -9,10 +9,12 @@ class SymbolTableModel : public ListingItemModel
     public:
         explicit SymbolTableModel(REDasm::ListingItemType itemtype, QObject *parent = nullptr);
         void setSymbolType(REDasm::SymbolType type);
+        void setSymbolFlags(REDasm::SymbolFlags flags);
 
     protected:
         bool isItemAllowed(const REDasm::ListingItem& item) const override;
 
     private:
-        REDasm::SymbolType m_symboltype;
+        REDasm::SymbolType m_symboltype{REDasm::SymbolType::None};
+        REDasm::SymbolFlags m_symbolflags{REDasm::SymbolFlags::None};
 };

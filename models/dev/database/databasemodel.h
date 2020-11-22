ccfc1e4f7a26931a13c84e208610eb4b32bc62c8
Dax <contact@antoniodavide.dev>
2020-11-22 19:30:07 +0100

    DatabaseModel: Revert changes

--- models/dev/database/databasemodel.h
+++ models/dev/database/databasemodel.h
@@ -1,54 +1,24 @@
 #pragma once
 
-#include <QAbstractListModel>
-#include <QJsonObject>
-#include <QJsonArray>
+#include <QAbstractItemModel>
 #include <rdapi/rdapi.h>
-#include <filesystem>
-#include <stack>
+#include <vector>
+#include "databasedatamodel.h"
 
-class DatabaseDataModel : public QAbstractListModel
+class DatabaseModel : public QAbstractListModel
 {
     Q_OBJECT
 
     public:
-        explicit DatabaseDataModel(RDDatabase* db, QObject *parent = nullptr);
-        QString currentQuery() const;
-        QString databaseName() const;
-        QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
-        QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-        int columnCount(const QModelIndex&) const override;
+        explicit DatabaseModel(QObject *parent = nullptr);
+        DatabaseDataModel* dataModel(const QModelIndex& index) const;
         int rowCount(const QModelIndex&) const override;
-        bool canGoBack() const;
-        bool canGoForward() const;
-        bool decompile(QByteArray& data);
-        void query(const QModelIndex& index);
-
-    public slots:
-        void goForward();
-        void goBack();
-        void queryRoot();
-        void query();
+        int columnCount(const QModelIndex&) const override;
+        QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+        QModelIndex addDatabase(RDDatabase* db);
 
     private:
-        void query(const std::filesystem::path &q);
-        QString objectValue(const QJsonValue& v) const;
-        bool isClickable(const QModelIndex& index) const;
-        QVariant objectData(const QModelIndex& index, int role) const;
-        QVariant arrayData(const QModelIndex& index, int role) const;
-        QVariant commonData(const QModelIndex& index, int role) const;
-
-    signals:
-        void backChanged();
-        void forwardChanged();
-        void queryChanged(const QString& query);
-
-    private:
-        std::stack<std::filesystem::path> m_back, m_forward;
-        RDDatabase* m_db;
-        std::filesystem::path m_query;
-        QStringList m_objkeys;
-        QJsonObject m_obj;
-        QJsonArray m_arr;
+        std::vector<rd_ptr<RDDatabase>> m_dblist;
+        std::unordered_map<RDDatabase*, DatabaseDataModel*> m_dbdata;
 };
 
